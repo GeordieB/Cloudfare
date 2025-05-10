1503f9e3f43be52f984474e811e931ce08c17d87
Geordie Buch-Frohlich <G.Buch.Frohlich@gmail.com>
2025-05-10 19:20:45 -0400

    Update import

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,8 @@
-import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { aliases, md } from 'vuetify/iconsets/md';
+import 'vuetify/styles/main.css';
 
 const vuetify = createVuetify({
   components,
