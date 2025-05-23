ce748ff8c52c1a6a63a303ddfa04196567186cdc
Geordie Buch-Frohlich <G.Buch.Frohlich@gmail.com>
2025-05-22 23:22:39 -0400

    Language fix

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -3,11 +3,18 @@ import en from '../assets/translations/en.json';
 import fr from '../assets/translations/fr.json';
 
 const i18n = createI18n({
-  locale: navigator.language.split('-')[0] || 'en',
+  locale: setLanguage(),
   messages: {
     en: en,
     fr: fr,
   },
 });
 
+function setLanguage() {
+  const language = navigator.language.split('-')[0].toLowerCase();
+  if (language !== 'en' && language !== 'fr') {
+    return 'en';
+  }
+  return language;
+}
 export default i18n;
