28f18a19a3f38260ede9654de4f021d6dccf4fae
Geordie Buch-Frohlich <G.Buch.Frohlich@gmail.com>
2025-05-23 00:32:38 -0400

    Design section

---
+++ src/components/Design.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="design-div px-8 pt-8">
+    <!--    Center between paragraphs like "Take up space"-->
+    <!--    <h1 class="pt-8 ml-16 font-weight-regular">-->
+    <!--      {{ $t('design.title') }}-->
+    <!--    </h1>-->
+    <p class="description mx-16">
+      {{ $t('design.description') }}
+    </p>
+    <br />
+    <div class="mx-16 ml-16 pl-16 pb-8">
+      <ul>
+        <li>
+          <p class="description mr-16">
+            {{ $t('design.firstBulletPoint') }}
+          </p>
+        </li>
+        <li>
+          <p class="description mr-16">
+            {{ $t('design.secondBulletPointBeforeLink') }}
+            <a href="https://naafa.org/sizefreedom">{{
+              $t('design.secondBulletPointLinkText')
+            }}</a>
+            {{ $t('design.secondBulletPointAfterLink') }}
+          </p>
+          <div class="mx-16 ml-16">
+            <br />
+            <ul>
+              <li>
+                <span class="description mr-16">
+                  <span class="description-yellow">
+                    {{ $t('design.yellowTitle') }}
+                  </span>
+                  {{ $t('design.yellowDescription') }}
+                </span>
+              </li>
+              <li>
+                <span class="description mr-16">
+                  <span class="description-white">
+                    {{ $t('design.whiteTitle') }}
+                  </span>
+                  {{ $t('design.whiteDescription') }}
+                </span>
+              </li>
+              <li>
+                <span class="description mr-16">
+                  <span class="description-coral px-1">
+                    {{ $t('design.coralTitle') }}
+                  </span>
+                  {{ $t('design.coralDescription') }}
+                </span>
+              </li>
+              <li>
+                <span class="description mr-16">
+                  <span class="description-blue">
+                    {{ $t('design.blueTitle') }}
+                  </span>
+                  {{ $t('design.blueDescription') }}
+                </span>
+              </li>
+              <li>
+                <span class="description mr-16">
+                  <span class="description-brown">
+                    {{ $t('design.brownTitle') }}
+                  </span>
+                  {{ $t('design.brownDescriptionBeforeLink') }}
+                  <a href="https://www.npr.org/transcripts/893006538">{{
+                    $t('design.brownDescriptionLinkText')
+                  }}</a>
+                  {{ $t('design.brownDescriptionAfterLink') }}
+                </span>
+              </li>
+            </ul>
+            <br />
+          </div>
+        </li>
+        <li>
+          <p class="description mr-16">
+            {{ $t('design.thirdBulletPointBeforeBold') }}
+            <span class="font-weight-bold">{{
+              $t('design.thirdBulletPointBold')
+            }}</span>
+            {{ $t('design.thirdBulletPointAfterBold') }}
+          </p>
+        </li>
+        <li>
+          <p class="description mr-16">
+            {{ $t('design.fourthBulletPoint') }}
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 40px;
+  transform: skewX(-10deg);
+}
+
+.design-div {
+  color: #201c37;
+  background-color: #ef734f;
+}
+
+.description {
+  font-size: 16px;
+  font-weight: 300;
+}
+
+.description-yellow {
+  font-weight: 500;
+  color: #ffca2e;
+}
+
+.description-white {
+  font-weight: 500;
+  color: #ffffff;
+}
+
+.description-coral {
+  font-weight: 500;
+  color: #ef734f;
+  background-color: #fcbaa7;
+}
+
+.description-blue {
+  font-weight: 500;
+  color: #0550cf;
+}
+
+.description-brown {
+  font-weight: 500;
+  color: #4f372d;
+}
+
+a {
+  color: #0550cf;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
 import HelloWorld from '../components/HelloWorld.vue';
 import Mission from '../components/Mission.vue';
+import Design from '../components/Design.vue';
+import { useDisplay } from 'vuetify/framework';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const display = useDisplay();
+const useFullSpace = ref(false);
+onMounted(async () => {
+  window.addEventListener('resize', () => handleResize());
+  handleResize();
+});
+
+onBeforeUnmount(async () => {
+  window.removeEventListener('resize', () => handleResize());
+});
+function handleResize() {
+  useFullSpace.value = !display.smAndDown.value;
+}
 </script>
 
 <template>
@@ -8,11 +25,35 @@ import Mission from '../components/Mission.vue';
   <hello-world />
   <v-spacer />
   <mission id="mission-block" />
+  <v-row
+    no-gutters
+    class="center"
+  >
+    <v-col class="d-flex justify-center">
+      <span
+        class="design-block text-pre-wrap align-center"
+        :style="{ 'font-size': useFullSpace ? '60px' : '40px' }"
+      >
+        {{ $t('design.title') }}
+      </span></v-col
+    ></v-row
+  >
+  <design id="design-block" />
   <v-spacer />
 </template>
 
 <style scoped>
 .v-spacer {
   padding-bottom: 200px;
+}
+
+.design-block {
+  color: white;
+  font-weight: 400;
+  font-style: normal; /* Ensure no default italic is applied */
+  transform: skewX(-10deg); /* Adjust the skew angle as needed */
+  display: inline-flex;
+  text-shadow: 5px 5px 5px #0550cf;
+  height: 200px;
 }
 </style>
