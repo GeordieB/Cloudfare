99ffd33b871f7dc0e522e0f12a345bb596cc0759
Geordie Buch-Frohlich <G.Buch.Frohlich@gmail.com>
2025-07-01 17:04:00 -0400

    fist logo

--- src/components/Mission.vue
+++ src/components/Mission.vue
@@ -5,28 +5,40 @@ import AnimatedComponent from './AnimatedComponent.vue';
 <template>
   <animated-component>
     <div class="mission-div px-8">
-      <h1 class="pt-8 ml-16 font-weight-regular">
-        {{ $t('mission.whatSection.title') }}
-      </h1>
-      <v-spacer class="py-2" />
-      <p class="description mx-16">
-        {{ $t('mission.whatSection.description') }}
-      </p>
-      <h1 class="pt-8 ml-16 font-weight-regular">
-        {{ $t('mission.flagSection.title') }}
-      </h1>
-      <v-spacer class="py-2" />
-      <p class="description mx-16 pb-4">
-        {{ $t('mission.flagSection.firstParagraph') }}
-      </p>
-      <p class="description mx-16 pb-8">
-        {{ $t('mission.flagSection.secondParagraphBeforeLink') }}
-        <a
-          href="https://www.instagram.com/sterling_azure/"
-          target="_blank"
-          >{{ $t('mission.flagSection.linkText') }}</a
-        >{{ $t('mission.flagSection.secondParagraphAfterLink') }}
-      </p>
+      <v-row no-gutters
+        ><v-col
+          cols="2"
+          class="d-flex justify-end align-center"
+        >
+          <img
+            src="../assets/fist_logo.png"
+            alt="fist-logo"
+            class="fist-logo" /></v-col
+        ><v-col>
+          <h1 class="pt-8 ml-16 font-weight-regular">
+            {{ $t('mission.whatSection.title') }}
+          </h1>
+          <v-spacer class="py-2" />
+          <p class="description mx-16">
+            {{ $t('mission.whatSection.description') }}
+          </p>
+          <h1 class="pt-8 ml-16 font-weight-regular">
+            {{ $t('mission.flagSection.title') }}
+          </h1>
+          <v-spacer class="py-2" />
+          <p class="description mx-16 pb-4">
+            {{ $t('mission.flagSection.firstParagraph') }}
+          </p>
+          <p class="description mx-16 pb-8">
+            {{ $t('mission.flagSection.secondParagraphBeforeLink') }}
+            <a
+              href="https://www.instagram.com/sterling_azure/"
+              target="_blank"
+              >{{ $t('mission.flagSection.linkText') }}</a
+            >{{ $t('mission.flagSection.secondParagraphAfterLink') }}
+          </p></v-col
+        ></v-row
+      >
     </div></animated-component
   >
 </template>
@@ -49,5 +61,10 @@ h1 {
 
 a {
   color: #0550cf;
+}
+
+.fist-logo {
+  height: auto;
+  width: 90%;
 }
 </style>
