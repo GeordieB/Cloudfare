1f7b4d89af6a7dd86d6707d57ca9f2e22d4c3e05
Geordie Buch-Frohlich <G.Buch.Frohlich@gmail.com>
2025-07-06 18:25:15 -0400

    Console

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,4 +1,5 @@
 export async function fetch(request: any, env: any) {
-  console.log(request);
+  console.log('request', request);
+  console.log('env', env);
   return new Response(env.GA_TAG_ID);
 }
